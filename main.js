c5dfe5d093ed82774730aaf53a054e997076f2af
yazeedalt <56513365+yazeedalt@users.noreply.github.com>
2019-12-01 18:07:04 +0300

    name

---
+++ main.js
@@ -0,0 +1,16 @@
+// $("#clicker").click(function() {
+//     $("#secret").fadeToggle();
+//     $("#clicker").text("Click me to toggle a hidden secret!");
+//     // $("#secret").slideDown();
+// });
+// ______________________________________________________________
+
+// $("#clicker").click(function() {
+//     $(".secret").slideUp();
+// });
+// ______________________________________________________________
+
+$("#clicker").click(function() {
+    $("img").attr("src", "https://camo.githubusercontent.com/cd497a9f2fde9bbaabd6af9c35135c55db70f7c5/687474703a2f2f72733238342e70627372632e636f6d2f616c62756d732f6c6c33362f426967737465766538372f476966732f4c697a5f4c656d6f6e5f486967685f466976652e6769667e63323030");
+});
+// ______________________________________________________________
